d22728d77f86423bc34812a9c1ae812a49d38648
yancie-yjr <57621290+yancie-yjr@users.noreply.github.com>
2020-03-29 21:47:03 +0800

    Update retinanet.py

--- mmdet/models/detectors/retinanet.py
+++ mmdet/models/detectors/retinanet.py
@@ -1,7 +1,7 @@
 from ..registry import DETECTORS
 from .single_stage import SingleStageDetector
 
-
+#装饰器装饰RetinaNet的功能，继承了单阶段的模型
 @DETECTORS.register_module
 class RetinaNet(SingleStageDetector):
 
